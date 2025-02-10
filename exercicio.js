5bb1455417db8c21e5804056cd643a4960257a9a
VinceMendneck <108556680+VinceMendneck@users.noreply.github.com>
2025-02-10 16:56:38 -0300

    Add files via upload

--- exercicio.js
+++ exercicio.js
@@ -1,38 +1,76 @@
-function Continente(continente) {
-    this.continente = continente;
+// Requisito: Crie uma classe de uma abstração:
+
+class Localidade {
+    constructor(continente, pais, estado) {
+        if (this.constructor === Localidade) {
+            throw new Error("Classe abstrata não pode ser instanciada diretamente.");
+        }
+        this.continente = continente;
+        this.pais = pais;
+        this.estado = estado;
+    }
+
+    formatar() {
+        throw new Error("Método 'formatar()' deve ser implementado.");
+    }
 }
 
-function Pais(pais) {
-    this.pais = pais;
+// Requisito: Crie pelo menos duas classes que sejam herdeiras da classe criada anteriormente:
+
+// Classe herdeira que usa continente e pais
+class Cidade extends Localidade {
+    constructor(continente, pais, nome) {
+        super(continente, pais, null); // Estado não é usado aqui
+        this.nome = nome;
+    }
+
+    formatar() {
+        return `${this.nome}, ${this.pais}, ${this.continente}`;
+    }
 }
 
-function Estado(estado){
-    this.estado = estado;
+// Classe herdeira que usa pais e estado
+class Distrito extends Localidade {
+    constructor(pais, estado, nome) {
+        super(null, pais, estado); // Continente não é usado aqui
+        this.nome = nome;
+    }
+
+    formatar() {
+        return `${this.nome}, ${this.estado}, ${this.pais}`;
+    }
 }
 
-function Localidade(continente, pais, estado){
-    Continente.call(this, continente);
-    Pais.call(this, pais); 
-    Estado.call(this, estado);
+// Classe para combinar as informações das duas herdeiras
+class Endereco {
+    constructor(cidade, distrito, rua, numero) {
+        this.cidade = cidade;
+        this.distrito = distrito;
+        this.rua = rua;
+        this.numero = numero;
+    }
+
+    formatarEndereco() {
+        // Combinando informações de Cidade e Distrito
+        return `${this.cidade.formatar()} - ${this.distrito.formatar()}\n${this.rua}, ${this.numero}`;
+    }
 }
 
-function Endereco(continente, pais, estado, cidade, rua, numero){
-    Localidade.call(this, continente, pais, estado);
-    this.cidade = cidade;
-    this.rua = rua;
-    this.numero = numero;
-}
+// Requisito: Crie pelo menos três instâncias de objetos:
 
-const endereco1 = new Endereco("América", "Brasil", "São Paulo", "Osasco", "Rua A", 123);
-const endereco2 = new Endereco("Europa", "Portugal", "Lisboa", "Campo Grande", "Rua B", 456);
-const endereco3 = new Endereco("África", "Angola", "Luanda", "Viana", "Rua C", 789);
+// Instâncias de objetos
+const cidade1 = new Cidade("América", "Brasil", "Osasco");
+const distrito1 = new Distrito("Brasil", "São Paulo", "Jardim Piratininga");
+const endereco1 = new Endereco(cidade1, distrito1, "Rua A", 123);
 
-const formatarEndereco = (endereco) => {
-    const { continente, pais, estado, cidade, rua, numero } = endereco;
-    return `${cidade}, ${estado}, ${pais}, ${continente}\n${rua}, ${numero}`;
-};
+const cidade2 = new Cidade("Europa", "Portugal", "Campo Grande");
+const distrito2 = new Distrito("Portugal", "Lisboa", "Alvalade");
+const endereco2 = new Endereco(cidade2, distrito2, "Rua B", 456);
 
-console.log('Endereço 1:\n', formatarEndereco(endereco1),'\n');
-console.log('Endereço 2:\n', formatarEndereco(endereco2),'\n');
-console.log('Endereço 3:\n', formatarEndereco(endereco3),'\n');
+const cidade3 = new Cidade("África", "Angola", "Viana");
+const distrito3 = new Distrito("Angola", "Luanda", "Talatona");
+const endereco3 = new Endereco(cidade3, distrito3, "Rua C", 789);
 
+console.log('Endereço 1:\n', endereco1.formatarEndereco(),'\n');
+console.log('Endereço 2:\n', endereco2.formatarEndereco(),'\n');
+console.log('Endereço 3:\n', endereco3.formatarEndereco(),'\n');
